a27eddcb00d0f5d443fb28e7fc248052cd2a5918
Maksim <maxnovikov51@gmail.com>
2018-03-27 11:57:27 +0300

    setup fomating for amount and created at

--- src/components/expenseItem.js
+++ src/components/expenseItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseItem = ({ id, description, amount, createdAt}) => (
   <div>
     <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-    <p>Amount: {amount} Created At: {new Date(createdAt).toString()}</p>
+    <p>
+      Amount: {numeral(amount/100).format('$0,0.00')} -
+      Created At: {moment(createdAt).format('Do MMMM, YYYY')}
+    </p>
   </div>
 );
 
